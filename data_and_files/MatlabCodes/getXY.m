a38526adab5defbcb9e78d1b93587aee48c94fd8
rkterence <rkterence@zju.edu.cn>
2018-12-04 22:01:32 +0800

    Version 1.0 of getXY.m, advanced data subtracting method

--- data_and_files/MatlabCodes/getXY.m
+++ data_and_files/MatlabCodes/getXY.m
@@ -4,18 +4,39 @@ function [xy_table, vars] = getXY(monthyear, category, prod_line, output_kind)
 % @param monthyear: A 2-by-1 string array. Example: ["201701"; "201711"]
 % @param category: The category of a specific kind of cigarette. Example:
 % "软红长嘴".
-% @param prod_line: the production line of the cigarette. Example: 'A'
+% @param prod_line: the production line of the cigarette. Example: 'A',
+% Note: A+E, B+D, C+F.
 % @param output_kind: to choose the output kind. 0 means leaf period, 1
 % means si period and 2 means the whole period (All production variables
 % and all test variables
 % @return xy_table: XY table
 % @return vars: The name of vars in production that has the same order as
 % xy_table.
+% @author: Terence Email: rkterence@zju.edu.cn
 %
-% NOTE: this script is in beta stage, but no overall testing has been made.
+% Related sub-functions: getHeader.m, getProductions.m,
+% getYearAndMonth.m. They are all called by this function.
+%
+% Version: 1.0. This version is published since Dec. 4th, 2018.
+%
+% Complexity of this algorithm in MATLAB:
+% several input kinds have been tested, and the result is acceptable, with
+% one of the most complex input (mode 2, and all months of data of category
+% 软红长嘴) costing about 5 mins on my laptop.
+%
+% NOTE: All functions of this script function has been tested, and
+% they have all passed the testing. The next version of this function will
+% fix the little bugs shown above, which is not considered primary now:
+% 1. Add productionorder info while subtracting from raw_data to avoid
+% other production line variables being added to the chosen prodline.
+% (Maybe through adding new column of productionorder info to raw_data, or,
+% say, StatisticsInProduction.xlsx.
 
 
-%% Load the latest preprocessed dataset:
+
+%-------------------------------------------------------------------------------
+% Load the latest preprocessed dataset:
+%-------------------------------------------------------------------------------
 % Load the relationship data and testing data
 load ..\data_and_files\raw_stats_charlie.mat ...
 relationship_charlie raw_fillsi raw_longsi raw_midsi raw_shortsi ...
@@ -38,26 +59,24 @@ for i=0:delta_month
         month = 1;
     end
 end
-disp("finished data loading")
 
-%% Get the production orders
+%-------------------------------------------------------------------------------
+% Get the production orders
+%-------------------------------------------------------------------------------
 % To get the production orders in the month year range:
 [productions, category_ex] = getProductions(relationship_charlie, monthyear, ...
     category, prod_line, output_kind);
 
-% ------------------------------------------------------------------------------
-% The code above has been successfully tested by input: 
-% getXY(["201510";"201602"], "软红长嘴", 'A', 1)
-% ------------------------------------------------------------------------------
-
-%% Get the xy_table:
+%-------------------------------------------------------------------------------
+% Get the xy_table:
+%-------------------------------------------------------------------------------
 % Get the number of production orders and variable names.
 num_prod = length(productions.Number);
-if input_kind ~= 2
-    vars = sort(unique(raw_data.Varname(string(rawdata.Category) == category_ex(1) ...
-        + string(raw_data.Category) == category_ex(2))));
+if output_kind == 2
+    vars = sort(unique(raw_data.Varname(logical((string(raw_data.Category) ... 
+        == category_ex(1)) + (string(raw_data.Category) == category_ex(2))))));
 else
-    vars = sort(unique(raw_data.Varname(string(rawdata.Category) ...
+    vars = sort(unique(raw_data.Varname(string(raw_data.Category) ...
     == category_ex)));
 end
 num_vars = length(vars);
@@ -70,16 +89,16 @@ i = 1;
 while i <= num_prod
     productionorder = productions.Productionorder(i);
     workorders = productions.Workorder(i);
-    while productions.Productionorder(i+1) == productionorder
+    while i < num_prod && productions.Productionorder(i+1) == productionorder
         workorders = [workorders; productions.Workorder(i+1)];
         i = i + 1;
     end
     row_value = []; row_variable = [];
     for k = 1:length(workorders)
         row_value = [row_value;
-            raw_data.Average(raw_data.Workorder == row_workorder(k))];
+            raw_data.Average(raw_data.Workorder == workorders(k))];
         row_variable = [row_variable;
-            raw_data.Varname(raw_data.Workorder == row_workorder(k))];
+            raw_data.Varname(raw_data.Workorder == workorders(k))];
     end
     num_row_variable = length(row_variable);
     row_vector = row_vector_default;
@@ -136,7 +155,7 @@ while i <= num_prod
     end
     % info stats
     row_vector{end-2} = workorders(1);
-    row_vector{end-1} = pruductionorder;
+    row_vector{end-1} = productionorder;
     row_vector{end} = category;
     % expand the xy_table
     xy_table = [xy_table; row_vector];
@@ -145,3 +164,109 @@ while i <= num_prod
 end
 xy_table = cell2table(xy_table, 'VariableNames', header);
 vars = vars';
+
+
+
+function [Header, row_vector_default] = getHeader(output_kind, n_vars)
+switch output_kind
+    case 0
+        Header = cell(1, n_vars + 7);
+        row_vector_default = cell(1, n_vars + 7);
+        for i = 1:n_vars
+            Header{i} = char("Var" + string(i));
+        end
+        Header{n_vars+1} = 'Bigleaf';
+        Header{n_vars+2} = 'Bmleaf';
+        Header{n_vars+3} = 'Midleaf';
+        Header{n_vars+4} = 'Brokenleaf';
+        Header{n_vars+5} = 'Workorder';
+        Header{n_vars+6} = 'Productionorder';
+        Header{n_vars+7} = 'Category';
+    case 1
+        Header = cell(1, n_vars+9);
+        row_vector_default = cell(1, n_vars + 9);
+        for i=1:n_vars
+            Header{i} = char("Var" + string(i));
+        end
+        Header{n_vars+1} = 'Fillsi';
+        Header{n_vars+2} = 'Longsi';
+        Header{n_vars+3} = 'Midsi';
+        Header{n_vars+4} = 'Shortsi';
+        Header{n_vars+5} = 'Brokensi';
+        Header{n_vars+6} = 'Completesi';
+        Header{n_vars+7} = 'Workorder';
+        Header{n_vars+8} = 'Productionorder';
+        Header{n_vars+9} = 'Category';
+    case 2
+        Header = cell(1, n_vars + 13);
+        row_vector_default = cell(1, n_vars + 13);
+        for i=1:n_vars
+            Header{i} = char("Var" + string(i));
+        end
+        Header{n_vars+1} = 'Bigleaf';
+        Header{n_vars+2} = 'Bmleaf';
+        Header{n_vars+3} = 'Midleaf';
+        Header{n_vars+4} = 'Brokenleaf';
+        Header{n_vars+5} = 'Fillsi';
+        Header{n_vars+6} = 'Longsi';
+        Header{n_vars+7} = 'Midsi';
+        Header{n_vars+8} = 'Shortsi';
+        Header{n_vars+9} = 'Brokensi';
+        Header{n_vars+10} = 'Completesi';
+        Header{n_vars+11} = 'Workorder';
+        Header{n_vars+12} = 'Productionorder';
+        Header{n_vars+13} = 'Category';
+end
+
+
+
+function [productions, category] = getProductions(relationship_charlie, ...
+    monthyear, category, prod_line, output_kind)
+
+% To get the production orders in the month year range:
+monthyear_array = char(relationship_charlie.Productionorder);
+monthyear_array = string(monthyear_array(:,1:6));
+startrow = find(monthyear(1) == monthyear_array, 1, 'first');
+endrow = find(monthyear(2) == monthyear_array, 1, 'last');
+productions = relationship_charlie(startrow:endrow, :);
+% Delete other production lines
+prod_line_array = char(productions.Productionorder);
+prod_line_array = prod_line_array(:, 9);
+productions(prod_line_array ~= prod_line, :) = []; 
+% Delete other categories
+switch output_kind
+    case 0
+        category = "利群(" + category + ")叶片";
+        productions(string(productions.Category) ~= category,:) = [];
+    case 1
+        category = "利群（" + category + "）叶丝";
+        productions(string(productions.Category) ~= category,:) = [];
+    case 2
+        category = ["利群(" + category + ")叶片"; "利群（" + category + ...
+            "）叶丝"];
+        productions=productions(logical((string(productions.Category)== ... 
+            category(1)) + (string(productions.Category) == category(2))), :);
+    otherwise
+        error("Wrong input of output_kind");
+end
+
+
+
+function [start_year, start_month, end_year, end_month] = getYearAndMonth ...
+    (monthyear)
+
+start_monthyear = char(monthyear(1));
+if start_monthyear(5) == '0'
+    start_month = start_monthyear(6) - '0';
+else
+    start_month = start_monthyear(6) - '0' + 10;
+end
+start_year = 15 + start_monthyear(4) - '5';
+
+end_monthyear = char(monthyear(2));
+if end_monthyear(5) == '0'
+    end_month = end_monthyear(6) - '0';
+else
+    end_month = end_monthyear(6) - '0' + 10;
+end
+end_year = 15 + end_monthyear(4) - '5';
